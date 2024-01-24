a582f0ac2365483cc89399ab20ec3785220d056a
iagoholekdev <augustinhocarrara43@gmail.com>
2024-01-23 23:27:15 -0300

    pequeno ajuste na validação

--- src/main/java/org/example/utils/responsability/StringHandler.java
+++ src/main/java/org/example/utils/responsability/StringHandler.java
@@ -5,6 +5,8 @@ import org.example.books.exception.NoBooksForThisGenreException;
 import org.example.utils.interfaces.ResponsibilityHandler;
 import org.example.utils.logger.AbstractLogger;
 
+import java.util.Objects;
+
 
 public abstract class StringHandler extends AbstractLogger implements ResponsibilityHandler<String> {
 
@@ -12,7 +14,7 @@ public abstract class StringHandler extends AbstractLogger implements Responsibi
 
     @Override
     public void handleResponsability(String userInput) throws NoBooksForThisGenreException {
-        if (nextHandler != null) {
+        if (Objects.nonNull(this.nextHandler)) {
             nextHandler.handleResponsability(userInput);
         }
     }
